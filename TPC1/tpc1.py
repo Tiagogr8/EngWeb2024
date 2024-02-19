d938eef094e7009f97ec50aafc592494e002ceb2
Tiagogr8 <tiagorodrigues1031@gmail.com>
2024-02-19 10:42:07 +0000

    tpc1

---
+++ TPC1/tpc1.py
@@ -0,0 +1,185 @@
+import xml.etree.ElementTree as ET
+import os
+
+
+html_template = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rua Information</title>
+    <style>
+        figure {
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+        figcaption {
+            font-style: italic;
+        }
+        figure img {
+            margin-top: 2rem;
+            max-width: 100%;
+            height: auto;
+            border-radius: 10px;
+            border: 2px solid #ddd;
+        }
+        figure img atual{
+            max-width: 50%;
+            height: auto;
+        }
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+'''
+
+if not os.path.exists("html"):
+    os.makedirs("html")
+
+xml_directory = "texto"
+
+for filename in os.listdir(xml_directory):
+    html = html_template
+    if filename.endswith(".xml"):
+        xml_file = os.path.join(xml_directory,filename)
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+
+        numero = root.find("./meta/número").text
+        nome = root.find("./meta/nome").text
+        html += "<h1> Rua " + numero + ": " + nome + "</h1>"
+
+        # texto
+        html+= "<div>\n"
+        for element in root.findall("./corpo/para"):
+            text = ""
+            if element is not None:
+                text += element.text or ""
+                for sub_element in element:
+                    sub_text = ""
+                    if sub_element.text:
+                        sub_text += sub_element.text
+                    if sub_element.tail:
+                        sub_text += sub_element.tail
+                    text += sub_text
+            html += "<p>" + text + "</p>\n"
+        html += "</div>\n"
+
+        html += "<div>\n"
+        for figura in root.findall("./corpo/figura"):
+            imagem = figura.find("imagem").attrib['path']
+            legenda = figura.find("legenda").text
+            html += f"<div style='margin-bottom: 20px;'>\n<figure>\n<img src=\"{imagem}\" alt=\"{legenda}\" style=\"max-width: 50%; height: auto;\">\n<figcaption>{legenda}</figcaption>\n</figure>\n</div>\n"
+        html += "</div>\n"
+
+        html += "<div>\n"
+        #### Casas
+        html += "<h2>Lista de Casas</h2>\n<table border='1'>\n<tr><th>Número</th><th>Enfiteuta</th><th>Foro</th><th>Descrição</th></tr>\n"
+        for casa in root.findall("./corpo/lista-casas/casa"):
+            numero_casa = casa.find("número").text if casa.find("número") is not None else ""
+            enfiteuta = casa.find("enfiteuta").text if casa.find("enfiteuta") is not None else ""
+            foro = casa.find("foro").text if casa.find("foro") is not None else ""
+            desc_element = casa.find("desc")
+            casa_desc = ""
+            if desc_element is not None:
+                for desc in desc_element:
+                    casa_desc = ""
+                    if desc is not None:
+                        casa_desc += desc.text or ""
+                        for d in desc:
+                            sub_text = ""
+                            if d.text:
+                                sub_text += d.text
+                            if d.tail:
+                                sub_text += d.tail
+                            casa_desc += sub_text
+            html += f"<tr><td>{numero_casa}</td><td>{enfiteuta}</td><td>{foro}</td><td>{casa_desc}</td></tr>\n"
+        html += "</table>\n"
+        html += "</div>\n"
+
+
+        imagens_rua = []
+        numero_rua_formatado = str(int(numero))
+
+        atual_directory = "atual"
+        for imagem in os.listdir(atual_directory):
+            if imagem.startswith(f"{numero_rua_formatado}-"):
+                caminho_img = os.path.join("../atual", imagem)
+                html += f"<div style='margin-bottom: 20px;'>\n<figure>\n<img src=\"{caminho_img}\" style=\"max-width: 50%; height: auto;\">\n\n</figure>\n</div>\n"
+
+        html += "<div>\n"
+        html += "<h4><a href='../index.html' style='text-decoration: none; color: black;'>Voltar ao Índice</a></h4>\n"
+        html += "</div>\n"
+
+        html += "</body>"
+        html += "</html>"
+        html_filename = os.path.splitext(os.path.basename(xml_file))[0] + ".html"
+        html_output_path = os.path.join("html", html_filename)
+        with open(html_output_path, "w", encoding="utf-8") as html_file:
+            html_file.write(html)
+        html_file.close()
+
+
+#### Index ####
+html_files = []
+html_directory = "html"
+for filename in os.listdir(html_directory):
+    if filename.endswith(".html"):
+        html_files.append(filename)
+
+index_html = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Índice de Ruas</title>
+    <style>
+        body {{
+            font-family: Arial, sans-serif;
+        }}c
+        h1 {{
+            color: black;
+        }}
+        ul {{
+            list-style-type: none;
+            padding: 0;
+        }}
+        li {{
+            margin-bottom: 10px;
+        }}
+        a {{
+            text-decoration: none; /* Remove o sublinhado padrão */
+            color: black; /* Muda a cor do texto para preto */
+            transition: color 0.3s; /* Adiciona uma transição suave na mudança de cor */
+        }}
+        a:hover {{
+            color: #0056b3; /* Muda a cor do texto quando o mouse está sobre o link */
+        }}
+    </style>
+</head>
+<body>
+    <h1>Índice de Ruas</h1>
+    <ul>
+"""
+
+for html_file in html_files:
+    rua_name = os.path.splitext(html_file)[0]
+    index_html += f"        <li><a href='html/{html_file}'>{rua_name}</a></li>\n"
+
+index_html += """
+    </ul>
+</body>
+</html>
+"""
+with open("index.html", "w") as index_file:
+    index_file.write(index_html)
